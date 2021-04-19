9c5d10d56614d2b1437596be736f8c8293c88b72
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-04-19 17:20:45 -0400

    Update index.ts

--- src/LIb/Auth/Google/index.ts
+++ src/LIb/Auth/Google/index.ts
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
-      callbackURL: "http://localhost:3001/api/users/google/callback",
+      callbackURL: "/api/users/google/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       try {
